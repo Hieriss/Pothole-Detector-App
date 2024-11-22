5dbe2614b4ab5fc042104e94c142d44928270ca2
Hieriss <tranminhhieeus@gmail.com>
2024-11-22 14:06:38 +0700

    update map

--- app/src/main/java/com/example/prj/MapPage.java
+++ app/src/main/java/com/example/prj/MapPage.java
@@ -1,7 +1,15 @@
 package com.example.prj;
 
+import static com.mapbox.maps.plugin.gestures.GesturesUtils.getGestures;
+import static com.mapbox.maps.plugin.locationcomponent.LocationComponentUtils.getLocationComponent;
+
 import android.animation.ValueAnimator;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -14,207 +22,145 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.prj.PlaceAutoSuggestAdapter;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.gestures.MoveGestureDetector;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.ImageHolder;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.LocationPuck2D;
+import com.mapbox.maps.plugin.gestures.OnMoveListener;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
-    private final int FINE_PERMISSION_CODE = 1;
-    private GoogleMap gMap;
-    private Marker currentLocationMarker;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    LocationCallback locationCallback;
-    Button searchButton, returnButton, searchbarButton;
-    RelativeLayout returnLayout, searchLayout;
+public class MapPage extends AppCompatActivity {
+    MapView mapView;
+    FloatingActionButton floatingActionButton;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean result) {
+            if (result) {
+                Toast.makeText(MapPage.this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MapPage.this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
+    private final OnIndicatorBearingChangedListener onIndicatorBearingChangedListener = new OnIndicatorBearingChangedListener() {
+        @Override
+        public void onIndicatorBearingChanged(double v) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().bearing(v).build());
+        }
+    };
+
+    private final OnIndicatorPositionChangedListener onIndicatorPositionChangedListener = new OnIndicatorPositionChangedListener() {
+        @Override
+        public void onIndicatorPositionChanged(@NonNull Point point) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().center(point).zoom(20.0).build());
+            getGestures(mapView).setFocalPoint(mapView.getMapboxMap().pixelForCoordinate(point));
+        }
+    };
+
+    private final OnMoveListener onMoveListener = new OnMoveListener() {
+        @Override
+        public void onMoveBegin(@NonNull MoveGestureDetector moveGestureDetector) {
+            getLocationComponent(mapView).removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+            getLocationComponent(mapView).removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+            getGestures(mapView).removeOnMoveListener(onMoveListener);
+            floatingActionButton.show();
+        }
+
+        @Override
+        public boolean onMove(@NonNull MoveGestureDetector moveGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public void onMoveEnd(@NonNull MoveGestureDetector moveGestureDetector) {
+
+        }
+    };
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else if (drawable instanceof VectorDrawable) {
+            VectorDrawable vectorDrawable = (VectorDrawable) drawable;
+            Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            vectorDrawable.draw(canvas);
+            return bitmap;
+        } else {
+            throw new IllegalArgumentException("Unsupported drawable type");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_map_page);
-
-        returnButton = findViewById(R.id.return_button);
-        searchButton = findViewById(R.id.search_button);
-
-        returnLayout = findViewById(R.id.return_button_layout);
-        searchLayout = findViewById(R.id.search_button_layout);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
+
+        mapView = findViewById(R.id.mapView);
+        floatingActionButton = findViewById(R.id.myLocationButton);
+        floatingActionButton.hide();
+
+        if(ActivityCompat.checkSelfPermission(MapPage.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
         }
-        PlacesClient placesClient = Places.createClient(this);
 
-        AutoCompleteTextView searchAddress = findViewById(R.id.search_address);
-
-        searchAddress.setAdapter(new PlaceAutoSuggestAdapter(this, android.R.layout.simple_list_item_1));
-        searchAddress.setOnItemClickListener((parent, view, position, id) -> {
-            String address = (String) parent.getItemAtPosition(position);
-            searchLocation(address);
-        });
-
-        searchAddress.setOnClickListener(new View.OnClickListener() {
+        mapView.getMapboxMap().loadStyleUri("mapbox://styles/hieriss/cm3ni7v2n00sl01sdeeoif31r", new Style.OnStyleLoaded() {
             @Override
-            public void onClick(View v) {
-                searchLayout.setVisibility(View.VISIBLE);
-                returnLayout.setVisibility(View.VISIBLE);
+            public void onStyleLoaded(@NonNull Style style) {
+                mapView.getMapboxMap().setCamera(new CameraOptions.Builder().zoom(20.0).build());
+                LocationComponentPlugin locationComponentPlugin = getLocationComponent(mapView);
+                locationComponentPlugin.setEnabled(true);
+                LocationPuck2D locationPuck2D = new LocationPuck2D();
+                Drawable drawable = AppCompatResources.getDrawable(MapPage.this, R.drawable.current_location);
+                Bitmap bitmap = getBitmapFromDrawable(drawable);
+                locationPuck2D.setBearingImage(ImageHolder.from(bitmap));
+                locationComponentPlugin.setLocationPuck(locationPuck2D);
+                locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                getGestures(mapView).addOnMoveListener(onMoveListener);
+
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                        getGestures(mapView).addOnMoveListener(onMoveListener);
+                        floatingActionButton.hide();
+                    }
+                });
             }
         });
-
-        searchAddress.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                    String address = searchAddress.getText().toString();
-                    searchLocation(address);
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchLayout.setVisibility(View.GONE);
-                returnLayout.setVisibility(View.GONE);
-            }
-        });
-
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String address = searchAddress.getText().toString();
-                searchLocation(address);
-
-                searchLayout.setVisibility(View.GONE);
-                returnLayout.setVisibility(View.GONE);
-            }
-        });
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
-        mapFragment.getMapAsync(this);
-
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    updateMapLocation(location);
-                }
-            }
-        };
-
-        requestLocationUpdates();
-    }
-    private void searchLocation(String address) {
-        Geocoder geocoder = new Geocoder(this);
-        try {
-            List<Address> addressList = geocoder.getFromLocationName(address, 1);
-            if (addressList != null && !addressList.isEmpty()) {
-                Address location = addressList.get(0);
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-                gMap.addMarker(new MarkerOptions()
-                        .position(latLng)
-                        .title(address)
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.search_marker)));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private void requestLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
-            return;
-        }
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(500); // 5 seconds
-        locationRequest.setFastestInterval(500); // 1 second
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-    }
-
-    private void updateMapLocation(Location location) {
-        if (gMap == null) {
-            return;
-        }
-        LatLng newLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-        float bearing = location.getBearing();
-        if (currentLocationMarker == null) {
-            currentLocationMarker = gMap.addMarker(new MarkerOptions()
-                    .position(newLatLng)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.current_location))
-                    .title("My Location")
-                    .rotation(bearing)
-                    .anchor(0.5f, 0.5f));
-            gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLatLng, 15));
-        } else {
-            animateMarkerTo(currentLocationMarker, newLatLng, bearing);
-        }
-    }
-
-    private void animateMarkerTo(final Marker marker, final LatLng newLatLng, final float bearing) {
-        final LatLng startLatLng = marker.getPosition();
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
-        valueAnimator.setDuration(1000); // 1 second
-        valueAnimator.addUpdateListener(animation -> {
-            float v = animation.getAnimatedFraction();
-            double lng = v * newLatLng.longitude + (1 - v) * startLatLng.longitude;
-            double lat = v * newLatLng.latitude + (1 - v) * startLatLng.latitude;
-            marker.setPosition(new LatLng(lat, lng));
-            marker.setRotation(bearing); // Update rotation
-        });
-        valueAnimator.start();
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        gMap = googleMap;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == FINE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                requestLocationUpdates();
-            } else {
-                Toast.makeText(this, "Permission Denied, Please Allow Permission", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
